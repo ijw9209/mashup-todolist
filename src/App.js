29a36d0744ecd633158e3bb48c4b26df4910dea3
ijw9209 <ijw9209@naver.com>
2022-05-25 00:22:05 +0900

    context

--- src/App.js
+++ src/App.js
@@ -85,6 +85,7 @@ import TodoTemplate from './components/TodoTemplate';
 import TodoHead from './components/TodoHead';
 import TodoList from './components/TodoList';
 import TodoCreate from './components/TodoCreate';
+import { TodoProvider } from './TodoContext';
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -96,12 +97,14 @@ const GlobalStyle = createGlobalStyle`
 const App = () => {
   return (
     <>
-      <GlobalStyle />
-      <TodoTemplate>
-        <TodoHead />
-        <TodoList />
-        <TodoCreate />
-      </TodoTemplate>
+      <TodoProvider>
+        <GlobalStyle />
+        <TodoTemplate>
+          <TodoHead />
+          <TodoList />
+          <TodoCreate />
+        </TodoTemplate>
+      </TodoProvider>
     </>
   );
 };
